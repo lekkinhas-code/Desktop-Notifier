0139dc2a79cb28a5dbd722627f1271541e63870b
lekkinhas-code <madalenadesousa.31@gmail.com>
2026-07-10 23:06:27 +0100

    Edit icons paths

--- app_paths.py
+++ app_paths.py
@@ -7,8 +7,6 @@ WORKER_FILE = os.path.join(BASE_DIR, "worker.py")
 
 DARK_THEME_FILE = os.path.join(BASE_DIR, "UI", "style", "darkTheme_styles.qss")
 LIGHT_THEME_FILE = os.path.join(BASE_DIR, "UI", "style", "lightTheme_styles.qss")
-# STATUS_COLUMN_STYLE_FILE = os.path.join(BASE_DIR, "UI", "style", "status_column.qss")
-
 
 BLACK_ICON_FILE = os.path.join(BASE_DIR, "image", "alarm_icon.ico")
 WHITE_ICON_FILE = os.path.join(BASE_DIR, "image", "alarm_icon_white.ico")
@@ -16,5 +14,8 @@ BLACK_ICON_FALLBACK_FILE = os.path.join(BASE_DIR, "image", "alarm_icon.png")
 WHITE_ICON_FALLBACK_FILE = os.path.join(BASE_DIR, "image", "alarm_icon_white.png")
 STATUS_ON_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "toggle_on.svg")
 STATUS_OFF_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "toggle_off.svg")
+EDIT_BLACK_BUTTON_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "edit_black.svg")
+EDIT_WHITE_BUTTON_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "edit_white.png")
+
 
 SOUND_FILE = os.path.join(BASE_DIR, "audio", "alarm_sound.wav")
