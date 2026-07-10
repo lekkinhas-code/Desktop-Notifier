09eca7bf875b9d9c49da6a38a5287280bd1079bf
lekkinhas-code <madalenadesousa.31@gmail.com>
2026-07-10 17:32:12 +0100

    path update

--- app_paths.py
+++ app_paths.py
@@ -7,10 +7,14 @@ WORKER_FILE = os.path.join(BASE_DIR, "worker.py")
 
 DARK_THEME_FILE = os.path.join(BASE_DIR, "UI", "style", "darkTheme_styles.qss")
 LIGHT_THEME_FILE = os.path.join(BASE_DIR, "UI", "style", "lightTheme_styles.qss")
+# STATUS_COLUMN_STYLE_FILE = os.path.join(BASE_DIR, "UI", "style", "status_column.qss")
+
 
 BLACK_ICON_FILE = os.path.join(BASE_DIR, "image", "alarm_icon.ico")
 WHITE_ICON_FILE = os.path.join(BASE_DIR, "image", "alarm_icon_white.ico")
 BLACK_ICON_FALLBACK_FILE = os.path.join(BASE_DIR, "image", "alarm_icon.png")
 WHITE_ICON_FALLBACK_FILE = os.path.join(BASE_DIR, "image", "alarm_icon_white.png")
+STATUS_ON_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "toggle_on.svg")
+STATUS_OFF_ICON_FILE = os.path.join(BASE_DIR, "UI", "images", "toggle_off.svg")
 
 SOUND_FILE = os.path.join(BASE_DIR, "audio", "alarm_sound.wav")
